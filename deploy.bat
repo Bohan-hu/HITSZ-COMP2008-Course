a5679488b2ff2706bfa2d04d7e20cd242f59a721
Bohan-Hu <hubohancser@gmail.com>
2020-03-31 15:43:44 +0800

    修正代码注释错误

--- deploy.bat
+++ deploy.bat
@@ -2,3 +2,12 @@ set /p message=commit_message:
 git add *
 git commit -m %message%
 git push 
+mkdocs build
+cd site
+git add *
+git commit -m %message%
+git push
+cd ../handouts
+git add *
+git commit -m %message%
+git push
